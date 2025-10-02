4a133c63706c027574a087a8f1c80243d09e4b78
IshimiYokoyama <milu80480@gmail.com>
2025-10-02 09:52:47 +0800

    Add: 修改 AgensGraph和ArangoDB 的 S2 查询

--- script/agensgraph/query/s2.sql
+++ script/agensgraph/query/s2.sql
@@ -1,22 +1,32 @@
 SET graph_path = academic_net;
-WITH PaperIDsInTopic AS (
-    SELECT (work_id_jsonb)::bigint AS work_id
-    FROM (
-        MATCH (w:work_v)-[:work_topic_e]->(t:topic_v)
-        WHERE t.display_name = 'Chemistry and Applications of Metal-Organic Frameworks'
-        RETURN w.id AS work_id_jsonb
-    ) AS t
+WITH topic_info AS (
+SELECT t.id,tv.vec
+FROM topic t
+JOIN topic_vec tv ON t.id = tv.id
+WHERE
+t.display_name = 'Chemistry and Applications of Metal-Organic Frameworks'
+),
+ranked_papers AS (
+SELECT (graph_match.work_id_jsonb)::bigint AS id,
+sqrt( (graph_match.edge_score_jsonb #>> '{}')::float / (wv.vec <-> ti.vec) ) AS topic_score
+FROM
+(
+MATCH (w:work_v)-[e:work_topic_e]->(t:topic_v)
+RETURN w.id AS work_id_jsonb,
+t.id AS topic_id_jsonb,
+e.score AS edge_score_jsonb
+) AS graph_match
+JOIN
+topic_info ti ON (graph_match.topic_id_jsonb)::bigint = ti.id
+JOIN
+work_vec wv ON (graph_match.work_id_jsonb)::bigint = wv.id
+ORDER BY
+topic_score DESC,id ASC
+LIMIT 10
 )
 SELECT
-    w.title,
-    w.cited_by_count,
-    w.publication_year,
-    w.language
+w.title,w.cited_by_count,rp.topic_score
 FROM
-    PaperIDsInTopic pit
-JOIN
-    work w ON pit.work_id = w.id
-ORDER BY
-    w.cited_by_count DESC,
-    w.title ASC
-LIMIT 10;
+ranked_papers rp
+JOIN work w ON w.id = rp.id
+ORDER BY rp.topic_score DESC,w.id ASC;
