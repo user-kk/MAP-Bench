3c0bd4df49eee965e72e420f3e2b933c41f0bb56
IshimiYokoyama <milu80480@gmail.com>
2025-10-16 13:17:29 +0800

    Add: 修改 HelmDB 的 S2 查询,将执行时间降低至10s左右

--- script/helmdb/query/s2.sql
+++ script/helmdb/query/s2.sql
@@ -1,18 +1,24 @@
 with topic_info as (
-    SELECT t.id,tv.vec
+    SELECT t.id as id,tv.vec as vec
         from topic t join topic_vec tv on t.id = tv.id
         where t.display_name = 'Chemistry and Applications of Metal-Organic Frameworks'
 ),
+tv_find AS(
+    SELECT  w.id as wid,ti.vec as tvec,e.score as sc
+    FROM topic_info ti,
+         work_topic_gra MATCH (w: work_v)-[e: work_topic_e]->(t: topic_v)
+    WHERE ti.id = t.id
+),
 related AS (
-    SELECT w.id,
-           sqrt((e.score)::float / (wv.vec <-> ti.vec)) AS topic_score
-    FROM topic_info ti, work_topic_gra MATCH (w: work_v)-[e: work_topic_e]->(t: topic_v),work_vec wv
-    WHERE ti.id = t.id and w.id = wv.id
-    ORDER BY sqrt((e.score)::float / (wv.vec <-> ti.vec)) DESC,w.id ASC
+    SELECT tv.wid as id,
+           sqrt((tv.sc)::float / (wv.vec <-> tv.tvec)) AS topic_score
+    FROM tv_find tv,work_vec wv
+    WHERE tv.wid = wv.id
+    ORDER BY sqrt((tv.sc)::float / (wv.vec <-> tv.tvec)) DESC,tv.wid ASC
     limit 10
 )
 SELECT w.title,
        w.cited_by_count,
        r.topic_score      
 FROM related r
-JOIN work w ON w.id = r.id
+JOIN work w ON w.id = r.id;
