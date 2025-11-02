260140bbefa7a1cb28c4171796b633e18aa0be41
hyh <2908065055@qq.com>
2025-11-02 18:32:34 +0800

    [feat]优化q14

--- script/agensgraph/query/q14.sql
+++ script/agensgraph/query/q14.sql
@@ -1,10 +1,18 @@
-SELECT a.id, a.display_name as author_name, jsonb_agg(DISTINCT g.title)::text AS titles,count(DISTINCT g.title) as paper_cnt
+with t as MATERIALIZED ( -- 需要加物化提示，告诉agensgraph id 很少 否则 au.id IN (SELECT to_jsonb(id) FROM t) 不会走索引
+    SELECT id as id
+    FROM   author_doc
+    WHERE  doc -> 'display_name_alternatives' @> '"Li Hongbo"'
+)
+
+SELECT a.id,
+       a.display_name                                          AS author_name,
+       jsonb_agg(DISTINCT g.title)::text                       AS titles,
+       count(DISTINCT g.title)                                 AS paper_cnt
 FROM (
         MATCH (au:author_v)<-[e:work_author_e]-(w:work_v)
+        WHERE au.id IN (SELECT to_jsonb(id) FROM t)
         RETURN au.id, w.title
-) g
-JOIN author_doc ad ON ad.id = g.id::bigint
-JOIN author a      ON a.id  = ad.id
-WHERE ad.doc -> 'display_name_alternatives' @> '"Li Hongbo"'
+     ) g
+JOIN author a ON a.id = g.id::bigint
 GROUP BY a.id, a.display_name
 ORDER BY a.id;
